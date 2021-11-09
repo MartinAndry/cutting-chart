b35d92d6a7c6a9628f185249ca6e518846169944
MartinAndry <martunenko13@ukr.net>
2021-11-09 15:18:29 +0200

    Add project files.

---
+++ cutting chart/authorization.Designer.cs
@@ -0,0 +1,266 @@
+﻿
+namespace cutting_chart
+{
+    partial class Authorization
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Authorization));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.TopBorder = new System.Windows.Forms.Panel();
+            this.CloseAppButton = new System.Windows.Forms.Label();
+            this.RightBorder = new System.Windows.Forms.Panel();
+            this.LeftBorder = new System.Windows.Forms.Panel();
+            this.DownBorder = new System.Windows.Forms.Panel();
+            this.BackGroundPic = new System.Windows.Forms.Panel();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.ForgotPassLabel = new System.Windows.Forms.Label();
+            this.RegistrationLabel = new System.Windows.Forms.Label();
+            this.PassField = new System.Windows.Forms.TextBox();
+            this.LoginField = new System.Windows.Forms.TextBox();
+            this.IconUser = new System.Windows.Forms.PictureBox();
+            this.IconPass = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.TopBorder.SuspendLayout();
+            this.BackGroundPic.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IconUser)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IconPass)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TopBorder
+            // 
+            this.TopBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(28)))), ((int)(((byte)(54)))));
+            this.TopBorder.Controls.Add(this.CloseAppButton);
+            this.TopBorder.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TopBorder.Location = new System.Drawing.Point(0, 0);
+            this.TopBorder.Name = "TopBorder";
+            this.TopBorder.Size = new System.Drawing.Size(496, 23);
+            this.TopBorder.TabIndex = 0;
+            this.TopBorder.MouseDown += new System.Windows.Forms.MouseEventHandler(this.TopBorder_MouseDown);
+            this.TopBorder.MouseMove += new System.Windows.Forms.MouseEventHandler(this.TopBorder_MouseMove);
+            // 
+            // CloseAppButton
+            // 
+            this.CloseAppButton.AutoSize = true;
+            this.CloseAppButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CloseAppButton.Font = new System.Drawing.Font("Segoe Script", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CloseAppButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(239)))), ((int)(((byte)(243)))), ((int)(((byte)(239)))));
+            this.CloseAppButton.Location = new System.Drawing.Point(474, -3);
+            this.CloseAppButton.Margin = new System.Windows.Forms.Padding(0);
+            this.CloseAppButton.Name = "CloseAppButton";
+            this.CloseAppButton.Padding = new System.Windows.Forms.Padding(0, 3, 3, 0);
+            this.CloseAppButton.Size = new System.Drawing.Size(22, 23);
+            this.CloseAppButton.TabIndex = 0;
+            this.CloseAppButton.Text = "X";
+            this.CloseAppButton.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            this.CloseAppButton.Click += new System.EventHandler(this.CloseAppButton_Click);
+            this.CloseAppButton.MouseEnter += new System.EventHandler(this.CloseAppButton_MouseEnter);
+            this.CloseAppButton.MouseLeave += new System.EventHandler(this.CloseAppButton_MouseLeave);
+            // 
+            // RightBorder
+            // 
+            this.RightBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(28)))), ((int)(((byte)(54)))));
+            this.RightBorder.Dock = System.Windows.Forms.DockStyle.Right;
+            this.RightBorder.Location = new System.Drawing.Point(491, 23);
+            this.RightBorder.Name = "RightBorder";
+            this.RightBorder.Size = new System.Drawing.Size(5, 415);
+            this.RightBorder.TabIndex = 1;
+            // 
+            // LeftBorder
+            // 
+            this.LeftBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(28)))), ((int)(((byte)(54)))));
+            this.LeftBorder.Dock = System.Windows.Forms.DockStyle.Left;
+            this.LeftBorder.Location = new System.Drawing.Point(0, 23);
+            this.LeftBorder.Name = "LeftBorder";
+            this.LeftBorder.Size = new System.Drawing.Size(5, 415);
+            this.LeftBorder.TabIndex = 2;
+            // 
+            // DownBorder
+            // 
+            this.DownBorder.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(18)))), ((int)(((byte)(28)))), ((int)(((byte)(54)))));
+            this.DownBorder.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.DownBorder.Location = new System.Drawing.Point(5, 433);
+            this.DownBorder.Name = "DownBorder";
+            this.DownBorder.Size = new System.Drawing.Size(486, 5);
+            this.DownBorder.TabIndex = 3;
+            // 
+            // BackGroundPic
+            // 
+            this.BackGroundPic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(117)))), ((int)(((byte)(187)))), ((int)(((byte)(253)))));
+            this.BackGroundPic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BackGroundPic.Controls.Add(this.pictureBox1);
+            this.BackGroundPic.Controls.Add(this.LoginButton);
+            this.BackGroundPic.Controls.Add(this.ForgotPassLabel);
+            this.BackGroundPic.Controls.Add(this.RegistrationLabel);
+            this.BackGroundPic.Controls.Add(this.PassField);
+            this.BackGroundPic.Controls.Add(this.LoginField);
+            this.BackGroundPic.Controls.Add(this.IconUser);
+            this.BackGroundPic.Controls.Add(this.IconPass);
+            this.BackGroundPic.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.BackGroundPic.Location = new System.Drawing.Point(5, 23);
+            this.BackGroundPic.Name = "BackGroundPic";
+            this.BackGroundPic.Size = new System.Drawing.Size(486, 410);
+            this.BackGroundPic.TabIndex = 4;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.LoginButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.LoginButton.FlatAppearance.BorderColor = System.Drawing.Color.WhiteSmoke;
+            this.LoginButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Moccasin;
+            this.LoginButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LoginButton.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginButton.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.LoginButton.Location = new System.Drawing.Point(185, 214);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 41);
+            this.LoginButton.TabIndex = 8;
+            this.LoginButton.Text = "Войти";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            // 
+            // ForgotPassLabel
+            // 
+            this.ForgotPassLabel.AutoSize = true;
+            this.ForgotPassLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.ForgotPassLabel.Font = new System.Drawing.Font("Comic Sans MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForgotPassLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.ForgotPassLabel.Location = new System.Drawing.Point(242, 271);
+            this.ForgotPassLabel.Name = "ForgotPassLabel";
+            this.ForgotPassLabel.Size = new System.Drawing.Size(123, 19);
+            this.ForgotPassLabel.TabIndex = 5;
+            this.ForgotPassLabel.Text = "Забыли пароль?";
+            // 
+            // RegistrationLabel
+            // 
+            this.RegistrationLabel.AutoSize = true;
+            this.RegistrationLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.RegistrationLabel.Font = new System.Drawing.Font("Comic Sans MS", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RegistrationLabel.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.RegistrationLabel.Location = new System.Drawing.Point(109, 271);
+            this.RegistrationLabel.Name = "RegistrationLabel";
+            this.RegistrationLabel.Size = new System.Drawing.Size(94, 19);
+            this.RegistrationLabel.TabIndex = 4;
+            this.RegistrationLabel.Text = "Регистрация";
+            // 
+            // PassField
+            // 
+            this.PassField.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PassField.Location = new System.Drawing.Point(149, 165);
+            this.PassField.Name = "PassField";
+            this.PassField.Size = new System.Drawing.Size(216, 29);
+            this.PassField.TabIndex = 3;
+            this.PassField.UseSystemPasswordChar = true;
+            // 
+            // LoginField
+            // 
+            this.LoginField.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LoginField.Location = new System.Drawing.Point(149, 104);
+            this.LoginField.Multiline = true;
+            this.LoginField.Name = "LoginField";
+            this.LoginField.Size = new System.Drawing.Size(216, 30);
+            this.LoginField.TabIndex = 2;
+            // 
+            // IconUser
+            // 
+            this.IconUser.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.IconUser.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("IconUser.BackgroundImage")));
+            this.IconUser.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.IconUser.Location = new System.Drawing.Point(113, 104);
+            this.IconUser.Name = "IconUser";
+            this.IconUser.Size = new System.Drawing.Size(30, 30);
+            this.IconUser.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.IconUser.TabIndex = 0;
+            this.IconUser.TabStop = false;
+            // 
+            // IconPass
+            // 
+            this.IconPass.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.IconPass.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("IconPass.BackgroundImage")));
+            this.IconPass.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.IconPass.Location = new System.Drawing.Point(113, 165);
+            this.IconPass.Name = "IconPass";
+            this.IconPass.Size = new System.Drawing.Size(30, 30);
+            this.IconPass.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.IconPass.TabIndex = 1;
+            this.IconPass.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(362, 165);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(39, 29);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Authorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(496, 438);
+            this.Controls.Add(this.BackGroundPic);
+            this.Controls.Add(this.DownBorder);
+            this.Controls.Add(this.LeftBorder);
+            this.Controls.Add(this.RightBorder);
+            this.Controls.Add(this.TopBorder);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.HelpButton = true;
+            this.Name = "Authorization";
+            this.Text = "authorization";
+            this.TopBorder.ResumeLayout(false);
+            this.TopBorder.PerformLayout();
+            this.BackGroundPic.ResumeLayout(false);
+            this.BackGroundPic.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.IconUser)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.IconPass)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel TopBorder;
+        private System.Windows.Forms.Panel RightBorder;
+        private System.Windows.Forms.Panel LeftBorder;
+        private System.Windows.Forms.Panel DownBorder;
+        private System.Windows.Forms.Panel BackGroundPic;
+        private System.Windows.Forms.Label CloseAppButton;
+        private System.Windows.Forms.PictureBox IconPass;
+        private System.Windows.Forms.PictureBox IconUser;
+        private System.Windows.Forms.Label ForgotPassLabel;
+        private System.Windows.Forms.Label RegistrationLabel;
+        private System.Windows.Forms.TextBox PassField;
+        private System.Windows.Forms.TextBox LoginField;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ cutting chart/authorization.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cutting_chart
+{
+    public partial class Authorization : Form
+    {
+        public Authorization()
+        {
+            InitializeComponent();
+
+            this.PassField.AutoSize = false;
+            this.PassField.Size = new Size(this.PassField.Size.Width, 30);
+        }
+
+        private void CloseAppButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void CloseAppButton_MouseEnter(object sender, EventArgs e)
+        {
+            CloseAppButton.ForeColor = Color.Red;
+        }
+
+        private void CloseAppButton_MouseLeave(object sender, EventArgs e)
+        {
+            CloseAppButton.ForeColor = Color.FromArgb(239, 243, 239);
+        }
+
+        Point lastPoint;
+        private void TopBorder_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Left += e.X - lastPoint.X;
+                this.Top += e.Y - lastPoint.Y;
+
+            }
+        }
+
+        private void TopBorder_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastPoint = new Point(e.X, e.Y);
+        }
+    }
+}
